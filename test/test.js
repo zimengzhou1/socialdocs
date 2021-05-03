860d255bb5a9bde87311aa0ff2c2ce4d5eb413ec
Zimeng Zhou <zimeng.zhou@gmail.com>
2021-05-04 00:49:28 +0300

    Add initial integration test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 const expect = require('chai').expect;
 const socketAPI = require('../socketAPI');
 const http = require('http');
+const io = socketAPI.io;
 
 describe("socketio server tests", function(){
     let server = undefined;
 
     before(function(done) {
 		server = http.createServer();
-        server.listen(4242);
-        var io = socketAPI.io;
+        server.listen(4242, () => { console.log("Setting up server")});
         io.attach(server)
 
         // Finished beforeAll setup
@@ -17,16 +17,26 @@ describe("socketio server tests", function(){
 
     after(function(done) {
 		if (server) {
-			server.on('close', () => { console.log('AFTER'); done(); });
-			server.close(() => { console.log('CLOSING'); server.unref(); });
+			server.on('close', () => { done(); });
+			server.close(() => { console.log('Closing server'); server.unref(); });
 		}
 	});
 
     it('Initial test', function(done) {
-		const client = require('socket.io-client')('http://localhost:4242/');
-		client.on('connection', () => console.log('Client connected'));
-        done();
+		const sender = require('socket.io-client')('http://localhost:4242/');
+        const receiver = require('socket.io-client')('http://localhost:4242/');
+        
+        const senderInput = "hello";
+        const senderId = "1234";
+        const senderUsername = "sender"
 
+        sender.emit('new_message', {inputToAdd: senderInput, id: senderId, username: senderUsername})
+		receiver.on('new_message', (data) => {
+            expect(data.inputToAdd).to.equal(senderInput, "Input message recieved should be the same as sent")
+            expect(data.id).to.equal(senderId, "Input id recieved should be the same as sent")
+            expect(data.username).to.equal(senderUsername, "Input username recieved should be the same as sent")
+        });
+        done();
 	});
 })
 
